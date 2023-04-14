4a5f5a6c3923119d5ad88b4fedb1aef8d3dc7b94
Kristian Elersic <ekristian238@gmail.com>
2023-04-14 11:44:48 +0200

    script file

---
+++ script.js
@@ -0,0 +1,67 @@
+
+
+//hover
+/*
+const colorButton = document.querySelector("#color-button");
+const HoversubNav = document.querySelector(".hover-sub-nav");
+
+colorButton.addEventListener("mouseover", () => {
+  HoversubNav.style.display = "block";
+});
+
+colorButton.addEventListener("mouseout", () => {
+  HoversubNav.style.display = "none";
+});
+*/
+
+  
+
+//button light up
+
+document.addEventListener('DOMContentLoaded', function() {
+        // Remove "active" class from all buttons
+        document.querySelectorAll('nav a').forEach(function(button) {
+            button.classList.remove('active');
+        });
+    
+    // Check which page we're on and activate the appropriate button
+    let currentPage = window.location.pathname.split('/').pop();
+    if (currentPage === 'index.html') {
+      document.querySelector('#home-button a').classList.add('active');
+    } else if (currentPage === 'digital.html') {
+      document.querySelector('#digital-button a').classList.add('active');
+    } else if (currentPage === 'analog.html') {
+      document.querySelector('#analog-button a').classList.add('active');
+    } else if (currentPage === 'about.html') {
+      document.querySelector('#about-button a').classList.add('active');
+    } else if (currentPage === 'color-gallery.html') {
+        document.querySelector('#digital-button a').classList.add('active');
+      document.querySelector('#color-button a').classList.add('active');
+    } else if (currentPage === 'bw-gallery.html') {
+        document.querySelector('#digital-button a').classList.add('active');
+      document.querySelector('#bw-button a').classList.add('active');
+    } else if (currentPage === 'color_23.html') {
+        document.querySelector('#digital-button a').classList.add('active');
+      document.querySelector('#color-button a').classList.add('active');
+      document.querySelector('#button-2023 a').classList.add('active');
+    } else if (currentPage === 'color_13.html') {
+        document.querySelector('#digital-button a').classList.add('active');
+      document.querySelector('#color-button a').classList.add('active');
+      document.querySelector('#button-2013 a').classList.add('active');
+    }
+  
+    // scrolling hiding navigation bar
+  // navigation bar scorilling
+  let prevScrollpos = window.pageYOffset;
+  window.onscroll = function() {
+    let currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("top-nav").classList.remove("hidden");
+    } else {
+      document.getElementById("top-nav").classList.add("hidden");
+    }
+    prevScrollpos = currentScrollPos;
+  }
+
+  });
+  
